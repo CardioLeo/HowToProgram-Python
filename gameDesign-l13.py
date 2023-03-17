180ca43fc2eccc628bd984bebf1d77cf7c216373
echocslsk <CardioLeo@gmail.com>
2023-03-17 03:39:10 -0600

    brings pseudo-code and python code up to the Dth card of pseudocode;

--- gameDesign-l13.py
+++ gameDesign-l13.py
@@ -1,7 +1,52 @@
-#
-#
-#
-#
-#
+# Author: John Keyser
+# Date Created:
+# Date Modified:
+# Description: This code was written down as I followed Keyser's video course,
+# titled, "How to Program: Concepts in Computer Science with Python Exercises,"
+# viewed through the Great Courses / Wondrium, using Kanopy through my local
+# library
+# Usage: none
+
+##########
 
 
+# card A
+
+# grid game
+
+# 3 steps:
+# 1 - initialize and set up
+# 2 - loop to make sure its not time to end the game
+# 3 - go through one round of the game
+
+# Card B
+
+# # initialize
+# # loop while game not over
+# # do a round of the game
+# 1 ^^ Encapsulate into comments ^^
+# 2 ^^ Then into functions ^^
+# "encapsulation into function"
+
+# Card C
+
+# def Initialization():
+    # # initialize game
+    # def return false if game should end, true if game is not over
+# def DoRound():
+    # # perform 1 round of game
+    # # initialize game
+    # initialize()
+    # # Loop while not over ContinueGame(): Do Round
+
+# Card Number D
+
+# Test!
+# Add print statements to make sure its all in order
+# def initialize 
+# # initialization game
+# print("initializing")
+# def Continue 
+
+
+##########
